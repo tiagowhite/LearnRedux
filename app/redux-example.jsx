c0596d9358b9066adf28b1cdad9a947f3fdd2e4e
Tiago Fernandes <tiagowhite@gmail.com>
2017-03-13 23:27:02 -0300

    Reducer and init redux'

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,3 +1,12 @@
 var redux = require('redux');
 
 console.log('Starting redux example');
+
+var reducers = (state = {name: 'Maria José'}, action) => {
+	return state;
+}
+
+var store = redux.createStore(reducers);
+
+var currentState = store.getState();
+console.log('currentState', currentState);
