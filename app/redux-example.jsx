a8dc3264c2d514b114e73b223d81a5ac2cbac11f
Tiago Fernandes <tiagowhite@gmail.com>
2017-03-15 23:41:30 -0300

    Adding redux devtools support

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,7 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducers = (state = stateDefault, action) => {
+var reducer = (state = {name: 'Eustacio'}, action) => {
 	switch (action.type) {
 		case 'CHANGE_NAME':
 			return {
@@ -14,12 +14,31 @@ var reducers = (state = stateDefault, action) => {
 	}
 }
 
-var store = redux.createStore(reducers);
-console.log('currentState', store.getState)
+var store = redux.createStore(reducer, redux.compose(
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+// subscribe to changes
+
+var unsubscribe = store.subscribe(() => {
+	var state = store.getState();
+
+	console.log('Name is: ', state.name);
+
+	document.getElementById('app').innerHTML = state.name;
+
+})
+
+//unsubscribe();
 
 store.dispatch({
 	type: 'CHANGE_NAME',
 	name: 'Tiago'
 });
 
-console.log('Name should be Tiago', store.getState());
+
+
+store.dispatch({
+	type: 'CHANGE_NAME',
+	name: 'Jubirula'
+});
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -20,7 +20,20 @@ var reducer = (state = stateDefault, action) => {
 	}
 }
 
-var store = redux.createStore(reducer)
+var store = redux.createStore(reducer,redux.compose(
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+
+var unsubscribe = store.subscribe(() => {
+	var state = store.getState();
+
+	console.log('searchText is: ', state.searchText);
+
+	document.getElementById('app').innerHTML = state.searchText;
+
+})
+
 
 console.log('currentState', store.getState());
 
@@ -29,4 +42,7 @@ store.dispatch({
 	type: 'CHANGE_SEARCH_TEXT',
 	searchText: 'Dog'
 })
-console.log('searchText should be dog', store.getState())
+store.dispatch({
+	type: 'CHANGE_SEARCH_TEXT',
+	searchText: 'Go to medic'
+})
