2987ed0e1b838448c0bf5e13cf8d919c086561fa
Tiago Fernandes <tiagowhite@gmail.com>
2017-03-19 02:16:33 -0300

    Add actions for add and remoe hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,43 +2,122 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: 'Eustacio'}, action) => {
-	switch (action.type) {
-		case 'CHANGE_NAME':
-			return {
-				...state,
-				name: action.name
-			}
-		default:
-		return state
-	}
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+}
+var nextHobbyId = 1;
+var nextMovieId = 1;
+
+var reducer = (state = stateDefault, action) => {
+
+  switch (action.type) {
+  case 'CHANGE_NAME':
+    return {
+      ...state,
+      name: action.name
+    }
+  case 'ADD_HOBBY':
+    return {
+      ...state,
+      hobbies: [
+        ...state.hobbies,
+        {
+          id: nextHobbyId++,
+          hobby: action.hobby
+        }
+      ]
+    }
+  case 'REMOVE_HOBBY':
+    return {
+      ...state,
+      hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+    }
+  case 'REMOVE_MOVIE':
+  return {
+    ...state,
+    movies: state.movies.filter((movies) => movies.id !== movies.id)
+  }
+
+  case 'ADD_MOVIE':
+    return {
+      ...state,
+      movies: [
+        ...state.movies,
+        {
+          id: nextMovieId++,
+          titlle: action.title,
+          genre: action.genre
+        }
+      ]
+    }
+  default:
+    return state
+  }
 }
 
 var store = redux.createStore(reducer, redux.compose(
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 
 // subscribe to changes
 
 var unsubscribe = store.subscribe(() => {
-	var state = store.getState();
+  var state = store.getState();
 
-	console.log('Name is: ', state.name);
+  console.log('Name is: ', state.name);
 
-	document.getElementById('app').innerHTML = state.name;
+  document.getElementById('app').innerHTML = state.name;
 
+  console.log('new state', store.getState());
 })
 
 //unsubscribe();
 
 store.dispatch({
-	type: 'CHANGE_NAME',
-	name: 'Tiago'
+  type: 'CHANGE_NAME',
+  name: 'Tiago'
 });
+
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Runing'
+})
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+})
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+})
+
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Jubirula'
+});
+
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Resevoir Dogs',
+  genre: 'Crime Drama'
+})
 
 
 
 store.dispatch({
-	type: 'CHANGE_NAME',
-	name: 'Jubirula'
-});
+  type: 'REMOVE_MOVIE',
+  id: 1
+})
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Gurdians of The galaxy',
+  genre: 'Action'
+})
